b3f81bc605377d9ac1e2ac208bbe59bf88b09d4b
Tshiamo Thekiso ST10132516 <st10132516@vcconnect.edu.za>
2024-10-11 09:04:10 +0200

    feat: add food monitoring page for birds of prey feat: add navigation to food monitoring page for birds of prey feat: display animals on birdsofprey home page

--- app/(tabs)/staff/animalgroup/birdsofprey/_layout.tsx
+++ app/(tabs)/staff/animalgroup/birdsofprey/_layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Tabs } from 'expo-router';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Icon from '@mdi/react';
+import { Icon as Icon2 } from 'react-native-elements';
 import { mdiBird } from '@mdi/js';
 
 export default function BirdsofPreyLayout() {
@@ -13,7 +14,10 @@ export default function BirdsofPreyLayout() {
 
             if (route.name === 'index') {
             return <Icon path={mdiBird} size={1} color={color} />;
-          } else if (route.name === 'profile') {
+          } else if (route.name === 'foodmonitoring'){
+            return <Icon2 name="restaurant" type="material" size={size} color={color} />;
+          }
+          else if (route.name === 'profile') {
             iconName = 'person';
           }
           return <Ionicons name={iconName} size={size} color={color} />;
@@ -24,6 +28,12 @@ export default function BirdsofPreyLayout() {
         name="index"
         options={{
           title: 'Birds of Prey',
+        }}
+      />
+      <Tabs.Screen
+        name = "foodmonitoring"
+        options={{
+          title: 'Food Monitoring',
         }}
       />
       <Tabs.Screen
